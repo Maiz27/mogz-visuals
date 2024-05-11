98112671a0d0d42d93028505fecf93fd9a1f7b90
Maged Faiz <magedfaiz98@gmail.com>
2024-05-11 16:42:06 +0200

    refactor: remove unused imports from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Header from '@/components/header/Header';
 import About from '@/components/sections/About';
 import Contact from '@/components/sections/Contact';
 import Services from '@/components/sections/Services';
 import ScrollHero from '@/components/sections/ScrollHero';
-import Footer from '@/components/footer/Footer';
 
 export default function Home() {
   return (
