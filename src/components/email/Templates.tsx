91ddb357766e9f9aa16c3a78dd4d31528251dae1
Maged Faiz <magedfaiz98@gmail.com>
2024-11-22 21:38:23 +0200

    feat: add fields to Contact email template

--- src/components/email/Templates.tsx
+++ src/components/email/Templates.tsx
@@ -1,14 +1,25 @@
 type ContactProps = {
   name: string;
   email: string;
+  date: string;
+  session: string;
   message: string;
 };
 
-export const Contact = ({ name, email, message }: ContactProps) => {
+export const Contact = ({
+  name,
+  email,
+  date,
+  session,
+  message,
+}: ContactProps) => {
   return (
     <div>
       <h1>Website Message from {name}</h1>
       <div>Email: {email}</div>
+
+      <div>Date: {date}</div>
+      <div>Session: {session}</div>
 
       <p>{message}</p>
     </div>
